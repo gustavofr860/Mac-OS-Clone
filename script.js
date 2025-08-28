6bb7a87f549a7e0a18eabf5e9e510b9e1f5b0967
gustavofr860 <168063671+gustavofr860@users.noreply.github.com>
2025-08-28 17:29:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let dock_apps = [
+    document.getElementById("finder"),
+    document.getElementById("launchpad"),
+    document.getElementById("calendar"),
+    document.getElementById("spotify"),
+    document.getElementById("notion"),
+    document.getElementById("chrome"),
+    document.getElementById("discord"),
+    document.getElementById("roblox"),
+    document.getElementById("github"),
+    document.getElementById("bin")
+]
+
+function zoom_dock_icon(){
+    
+}
